90587fc8fad9730f18bcf1b9964756fbfe695417
andemar <andemartinez98@hotmail.com>
2019-10-21 11:19:03 -0500

    clase 25

--- src/main/java/com/platzi/ereservation/vista/resources/ClienteResource.java
+++ src/main/java/com/platzi/ereservation/vista/resources/ClienteResource.java
@@ -1,7 +1,11 @@
 package com.platzi.ereservation.vista.resources;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -80,5 +84,36 @@ public class ClienteResource {
 		// retorna el cliente update y un status.
 		return new ResponseEntity<>(this.clienteService.update(cliente), HttpStatus.OK);
 	}
+	
+	/**
+	 * Con @DeleteMapping("/{id}") se expecifica que es un servicio de delete, el cual
+	 * recibe el id del cliente a delete.
+	 * @param identificacion
+	 */
+	@DeleteMapping("/{identificacion}")
+	public void removeCliente(@PathVariable("identificacion") String identificacion) {
+		Cliente cliente = this.clienteService.findByIdentificacion(identificacion);
+		
+		if(cliente != null) {
+			this.clienteService.delete(cliente);
+		}
+	}
+	
+	
+	/**
+	 * Con @GetMapping se expecifica que es un servicio que retorna el resultado de una
+	 * consulta, en este caso una lista de clientes.
+	 * @return
+	 */
+	@GetMapping
+	public ResponseEntity<List<Cliente>> findAll(){
+		return ResponseEntity.ok(this.clienteService.findAll());
+	}
+	
+	
+	
+	
+	
+	
 
 }
